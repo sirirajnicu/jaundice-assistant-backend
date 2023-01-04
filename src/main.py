d7631720d8532bc52b8ab672b4f6726aed9054db
nawat <n.ngerncham@gmail.com>
2023-01-04 13:35:44 +0700

    feat: boiler plate code for each page added

--- src/main.py
+++ src/main.py
@@ -8,5 +8,25 @@ def main():
     return render_template("main.html")
 
 
+@app.route("/current-patient/")
+def current_patient():
+    return render_template("views/current-patient.html")
+
+
+@app.route("/consultation-status/")
+def consultation_status():
+    return render_template("views/consultation-status.html")
+
+
+@app.route("/order-review/")
+def order_review():
+    return render_template("views/order-review.html")
+
+
+@app.route("/case-review/")
+def case_review():
+    return render_template("views/case-review.html")
+
+
 def run():
     app.run(debug=True, use_reloader=False)
