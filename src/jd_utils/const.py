c8eaabb057a406b48b5d28a06812ced3671d8db0
nawat <n.ngerncham@gmail.com>
2023-01-05 15:57:27 +0700

    feat: added util functions for risk splitting

---
+++ src/jd_utils/const.py
@@ -0,0 +1,12 @@
+HYPERBILIRUBINEMIA_RISKS = [
+    "GA < 38 weeks",
+    "Jaundice within 24 hours",
+    "Pre-discharge TCB close to PT",
+    "Hemolysis or suspected",
+    "Phototherapy before discharge",
+    "History of parents or sibling requiring treatment",
+    "Suboptimal breastfeeding",
+    "Scalp hematoma or significant bruising",
+    "Down syndrome",
+    "Macrosomic infant of diabetic mother"
+]
