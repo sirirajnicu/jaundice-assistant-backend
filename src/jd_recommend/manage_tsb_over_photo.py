90c710f57ddc2a784be4007488fdca2aafcef083
Nawat Ngerncham <n.ngerncham@gmail.com>
2023-08-04 17:50:55 +0700

    feat: handler when tsb close to exchange threshold

--- src/jd_recommend/manage_tsb_over_photo.py
+++ src/jd_recommend/manage_tsb_over_photo.py
@@ -2,6 +2,9 @@ from src.jd_models import Patient, PhototherapyType
 
 
 def handle_far_below_threshold(patient: Patient) -> str or list[str]:
+    """
+    Based on Figure 4 - left arm
+    """
     treatments = [
         "Follow TSB/shielded TCB for 12-24 hours based on age and TCB/TSB trajectory",
         "PE and work for causes"
@@ -17,8 +20,36 @@ def handle_far_below_threshold(patient: Patient) -> str or list[str]:
     return treatments
 
 
+# TODO: make a pop-up on user screen when this is called
+def request_bind_score() -> float:
+    return 0.0
+
+
 def handle_close_below_threshold(patient: Patient) -> str or list[str]:
-    pass
+    """
+    Based on Figure 4 - middle arm
+    """
+    treatments = []
+    match patient.on_photo_therapy:
+        case PhototherapyType.NONE:
+            treatments.append("Put on double photo STAT")
+        case PhototherapyType.SINGLE:
+            treatments.append("Consider switching to double photo")
+        case PhototherapyType.DOUBLE:
+            treatments.append("Consider increasing photo intensity")
+
+    if request_bind_score() >= 4:
+        treatments.append("Do an exchange transfusion")
+    else:
+        treatments.extend([
+            "Continue phototherapy",
+            "Consider exchange transfusion if A/B ratio is high",
+            "NPO, peripheral IV",
+            "Cross-matched (1-2 hours of waiting time)",
+            "PE and work up for cause",
+            "Follow up on TSB values every 2 hours until > 2mg/dL below exchange threshold",
+            "Consider IVIG in infants with DAT positive"
+        ])
 
 
 def manage_tsb_over_threshold(patient: Patient, exchange_threshold: float) -> str or list[str]:
