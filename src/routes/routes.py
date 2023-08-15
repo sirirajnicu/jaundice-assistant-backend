dee79f42ed5df07905d169bf9f283b0ab40325af
Nawat Ngerncham <n.ngerncham@gmail.com>
2023-08-15 12:59:45 +0700

    fix: prep routes for backend+deleted html templates

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,7 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
-
-from src.jd_utils import split_risks
-from src.jd_utils.const import HYPERBILIRUBINEMIA_RISKS, NEUROTOXICITY_RISKS
+from flask import Blueprint, redirect, url_for
 
 routes = Blueprint("routes", __name__)
 
@@ -13,22 +10,19 @@ def main():
 
 @routes.route("/current-patient/")
 def current_patient():
-    hyper_risks: list[list[str]] = split_risks(HYPERBILIRUBINEMIA_RISKS)
-    neuro_risks: list[list[str]] = split_risks(NEUROTOXICITY_RISKS)
-    return render_template("views/current-patient.html", risks1=hyper_risks, risks2=neuro_risks,
-                           is_risky1=True, is_risky2=False)
+    pass
 
 
 @routes.route("/consultation-status/")
 def consultation_status():
-    return render_template("views/consultation-status.html")
+    pass
 
 
 @routes.route("/order-review/")
 def order_review():
-    return render_template("views/order-review.html")
+    pass
 
 
 @routes.route("/case-review/")
 def case_review():
-    return render_template("views/case-review.html")
+    pass
