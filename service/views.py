51981c57b1a62b884eee435d52f4ecc595882533
Tiosphere <loong_22@hotmail.com>
2023-09-23 08:58:47 +0700

    formating and add upbutton

--- service/views.py
+++ service/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
-from service.forms import LoginForm, SearchForm
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as authlogin
 from django.contrib.auth import logout as authlogout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
+from service.forms import LoginForm, SearchForm
 
 
 class RequestMethod(enumerate):
@@ -27,7 +28,7 @@ def login(request: HttpRequest) -> HttpResponse:
             password: str = form.cleaned_data["password"]
             role: str = form.cleaned_data["role"]
             user = authenticate(request, username=username, password=password)
-            print(user)
+            print(user, password, role)
             if user is not None:
                 authlogin(request, user)
                 request.session["role"] = role
@@ -47,16 +48,17 @@ def logout(request: HttpRequest) -> HttpResponse:
 
 @login_required
 def HNsearch(request: HttpRequest) -> HttpResponse:
-    ANmap = {"1":[1.1, 1.2, 1.3], "2":[2.1, 2.2],"3":[]}
+    MockANmap = {"1": [1.1, 1.2, 1.3], "2": [2.1, 2.2], "3": []}
     data = {
         "SearchForm": SearchForm(auto_id=False),
+        "pageName": "Create/Search",
     }
     if request.method == RequestMethod.POST:
         form = SearchForm(data=request.POST)
         if form.is_valid():
             searchid = form.cleaned_data["searchid"]
             data["HN"] = searchid
-            data["ANlist"] = ANmap[data["HN"]]
+            data["ANlist"] = MockANmap[data["HN"]]
             messages.error(request, "Invalid search term.", extra_tags="error")
         else:
             messages.error(request, "Invalid search term.", extra_tags="error")
