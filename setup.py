57913fa149f375cadf9a1a70885b9dc2a41785fe
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2024-05-15 22:50:20 +0700

    feat: docker compose file and cleaned dependencies

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
-import secrets
-import yaml
-import string
 import os
-import datetime
+import argparse
+import random
+import string
+from datetime import datetime
+
+
+def write_secret(file_path: str, content: str):
+    if os.path.exists(file_path):
+        return
+
+    with open(file_path, "w") as f:
+        f.write(content)
+
+
+def random_string(k: int):
+    valid_string = string.ascii_letters + string.digits + string.punctuation
+    return f"{datetime.now().timestamp()}-{''.join([random.choice(valid_string) for _ in range(k)])}"
 
 
 if __name__ == "__main__":
-    valid_string: str = string.ascii_letters + string.punctuation + string.digits
-    CRED = {"DEBUG": True}
-    if os.path.exists("cred.yaml"):
-        CRED = yaml.load(open("cred.yaml"), Loader=yaml.Loader)
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("--dev", action="store_true", help="Set up development environment")
+    args = args_parser.parse_args()
 
-    new_key = "".join([secrets.choice(valid_string) for _ in range(50)])
-    SECRET_KEY = f"secure-{datetime.datetime.now().timestamp()}-{new_key}"
-    lines: list[str] = [
-        # Production
-        f"DEBUG: {CRED['DEBUG']}",
-        f"SECRET_KEY: {SECRET_KEY}",
-    ]
-    with open("new_cred.yaml", mode="w", encoding="utf-8") as file:
-        file.write("\n".join(lines))
+    if args.dev:
+        write_secret("./secrets/db_password.txt", "postgres")
+        write_secret("./secrets/django_secrets.txt", "secretkey")
+    else:
+        write_secret("./secrets/db_password.txt", random_string(40))
+        write_secret("./secrets/django_secrets.txt", random_string(40))
